5744f8ad68670fd91143d9bfb87514ca3ddb45b9
Xande-Alves <alexandre.upe@gmail.com>
2025-03-28 11:51:25 -0300

    axios e logica de envio de dados para a API na pagina Quero Doar

--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,9 +1,48 @@
-import s from './queroDoar.module.scss';
-import frame from '../../assets/Frame.png';
-import Footer from '../../Components/Footer/Footer';
-import Header from '../../Components/Header/Header';
+import s from "./queroDoar.module.scss";
+import frame from "../../assets/Frame.png";
+import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header/Header";
+import { useState } from "react";
+import axios from "axios";
 
-export default function QueroDoar() { 
+export default function QueroDoar() {
+  const [titulo, setTitulo] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [autor, setAutor] = useState("");
+  const [imagem_url, setImagem_url] = useState("");
+
+  const capturarTitulo = (e) => {
+    setTitulo(e.target.value);
+  };
+  const capturarCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+  const capturarAutor = (e) => {
+    setAutor(e.target.value);
+  };
+  const capturarImagemURL = (e) => {
+    setImagem_url(e.target.value);
+  };
+
+  const enviarDados = async () => {
+    const endPointAPI = "https://api-livros-udtp.onrender.com/doar";
+
+    const dadosAEnviar = {
+      titulo,
+      categoria,
+      autor,
+      imagem_url,
+    };
+
+    try {
+      const resposta = await axios.post(endPointAPI, dadosAEnviar);
+      alert("Livro cadastrado com sucesso!");
+    } catch (erro) {
+      console.error("Erro ao cadastrar livro:", erro);
+      alert("Erro ao cadastrar livro. Verifique os dados e tente novamente.");
+    }
+  };
+
   return (
     <>
       <Header />
@@ -18,12 +57,27 @@ export default function QueroDoar() {
             <h2>Informações do Livro</h2>
           </section>
           <section className={s.sectionInputDoar}>
-            <input type="text" placeholder="Título" />
-            <input type="text" placeholder="Categoria" />
-            <input type="text" placeholder="Autor" />
-            <input type="text" placeholder="Link da Imagem" />
+            {/* <form onSubmit={(e) => e.preventDefault()}> */}
+              <input
+                type="text"
+                placeholder="Título"
+                onChange={capturarTitulo}
+              />
+              <input
+                type="text"
+                placeholder="Categoria"
+                onChange={capturarCategoria}
+              />
+              <input type="text" placeholder="Autor" onChange={capturarAutor} />
+              <input
+                type="text"
+                placeholder="Link da Imagem"
+                onChange={capturarImagemURL}
+              />
+              {/* <button onClick={enviarDados}>Doar</button> */}
+            {/* </form> */}
           </section>
-          <button>Doar</button>
+          <button onClick={enviarDados}>Doar</button>
         </section>
       </section>
       <Footer />
